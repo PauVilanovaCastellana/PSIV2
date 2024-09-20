827999f845decf91d3874b2eeb8273a4bcfaae65
Eduard Ara Paredes <91468577+NIU1636341@users.noreply.github.com>
2024-09-20 12:38:22 +0200

    Add files via upload

---
+++ REPTE 1/Localizacio/localizacionv3.py
@@ -0,0 +1,239 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 19:53:59 2024
+
+@author: eduar
+"""
+
+import torch
+from PIL import Image, ImageDraw, ImageFont
+import cv2
+import numpy as np
+import os
+import pytesseract
+import warnings
+warnings.simplefilter("ignore", category=FutureWarning)
+
+# Asegúrate de que la ruta de Tesseract esté correctamente configurada
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Cambia la ruta si es necesario
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+def load_model(model_path):
+    """
+    Carga el modelo YOLO desde la ruta proporcionada.
+    """
+    return torch.hub.load('ultralytics/yolov5', 'custom', path=model_path, force_reload=True)
+
+def load_image(img_path):
+    """
+    Carga una imagen desde la ruta proporcionada.
+    """
+    return cv2.imread(img_path)
+
+def detect_objects(model, img):
+    """
+    Realiza la detección de objetos en la imagen utilizando el modelo.
+    """
+    results = model(img)
+    return results.xyxy[0].cpu().numpy()
+
+def get_best_detection(detections):
+    """
+    Obtiene la detección con mayor confianza de entre las detecciones proporcionadas.
+    """
+    if len(detections) > 0:
+        return max(detections, key=lambda x: x[4])
+    return None
+
+def draw_detection(img, detection):
+    """
+    Dibuja el recuadro y el texto "Matrícula" sobre la imagen para la detección proporcionada.
+    Además, devuelve las coordenadas del recuadro.
+    """
+    x1, y1, x2, y2, _, _ = map(int, detection)
+    
+    # Dibujar el recuadro en la imagen OpenCV
+    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    
+    # Convertir la imagen de OpenCV (BGR) a PIL (RGB)
+    img_pil = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(img_pil)
+
+    # Cargar la fuente de PIL
+    font_path = "C:/Windows/Fonts/arial.ttf"  # Usa una fuente válida en tu sistema
+    font_size = 36
+    try:
+        font = ImageFont.truetype(font_path, font_size)
+    except IOError:
+        print(f"Error: No se puede abrir la fuente en la ruta {font_path}.")
+        font = ImageFont.load_default()
+    
+    # Calcular el tamaño del texto
+    text = 'Matrícula'
+    bbox = draw.textbbox((0, 0), text, font=font)
+    text_width = bbox[2] - bbox[0]
+    text_height = bbox[3] - bbox[1]
+    
+    # Calcular la posición para centrar el texto
+    text_x = x1 + (x2 - x1 - text_width) / 2
+    text_y = y1 - text_height - 10
+    
+    # Dibujar el texto centrado en la imagen PIL
+    draw.text((text_x, text_y), text, font=font, fill=(255, 0, 0))
+    
+    # Convertir de nuevo la imagen PIL a OpenCV (BGR)
+    img = cv2.cvtColor(np.array(img_pil), cv2.COLOR_RGB2BGR)
+    return img, (x1, y1, x2, y2)
+
+def recortar_imagen_con_coordenadas(image, coords, margen=0):
+    """
+    Recorta la imagen usando las coordenadas proporcionadas y agrega un margen opcional.
+    """
+    x1, y1, x2, y2 = coords
+
+    # Agregar margen
+    x1 = max(0, x1 - margen)
+    y1 = max(0, y1 - margen)
+    x2 = min(image.shape[1], x2 + margen)
+    y2 = min(image.shape[0], y2)
+
+    # Recortar la imagen usando slicing en NumPy
+    recorte = image[y1:y2, x1:x2]
+    return recorte
+
+def extraer_canal_v(img_crop):
+    """
+    Convierte una imagen al espacio HSV, extrae solo el canal V y muestra la imagen resultante.
+    """
+    # Convertir la imagen a HSV
+    hsv = cv2.cvtColor(img_crop, cv2.COLOR_BGR2HSV)
+    
+    # Extraer el canal V
+    canal_v = hsv[:, :, 2]
+    
+    # Mostrar el canal V
+    cv2.imshow('Canal V', canal_v)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    return canal_v
+
+def procesar_y_enderezar_letras(img_crop):
+    if img_crop is None or img_crop.size == 0:
+        raise ValueError("La imagen no se ha cargado correctamente o está vacía.")
+    
+    # Convertir la imagen a escala de grises
+    img_gray = cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY)
+    img_blur = cv2.GaussianBlur(img_gray, (5, 5), 0)
+    
+    # Extraer el canal V (valor) y aplicar Canny
+    hsv = cv2.cvtColor(img_crop, cv2.COLOR_BGR2HSV)
+    canal_v = hsv[:, :, 2]
+    bordes_canny = cv2.Canny(canal_v, 30, 100)  # Ajusta estos valores
+
+    # Encontrar contornos
+    contornos, _ = cv2.findContours(bordes_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    if not contornos:
+        raise ValueError("No se detectaron contornos en la imagen.")
+    
+    # Filtrar los contornos por tamaño
+    contornos_letras = []
+    for contorno in contornos:
+        area = cv2.contourArea(contorno)
+        if area > 50:  # Ajusta este valor según sea necesario
+            contornos_letras.append(contorno)
+    
+    if not contornos_letras:
+        raise ValueError("No se encontraron contornos significativos que representen letras o números.")
+    
+    # Enderezamiento (igual que antes)
+    contornos_todos = np.vstack(contornos_letras)
+    rect = cv2.minAreaRect(contornos_todos)
+    angulo = rect[2]
+    print(angulo)
+    # Ajustar el ángulo para que las letras queden completamente horizontales
+    if angulo < 15:
+        angulo= angulo
+    elif angulo < -45:
+        angulo = 90 + angulo
+    elif angulo > 45:
+        angulo = angulo - 90
+    elif angulo < 60:
+        angulo = 15
+
+
+    (h_img, w_img) = img_crop.shape[:2]
+    centro = (w_img // 2, h_img // 2)
+    M = cv2.getRotationMatrix2D(centro, angulo, 1.0)
+    img_rotada = cv2.warpAffine(img_crop, M, (w_img, h_img), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    
+    return img_rotada
+
+def resize_image(img, max_width=760, max_height=232):
+    """
+    Redimensiona la imagen para que se ajuste a las dimensiones máximas especificadas.
+    """
+    height, width = img.shape[:2]
+    scaling_factor = min(max_width / width, max_height / height)
+    new_width = int(width * scaling_factor)
+    new_height = int(height * scaling_factor)
+    return cv2.resize(img, (new_width, new_height)), scaling_factor
+
+
+def main():
+    # Configuración
+    model_path = 'yolov5/runs/train/exp5/weights/best.pt'
+    directorio_imagenes = os.path.dirname(os.path.abspath(__file__))  # Carpeta donde está el archivo
+    extensiones = ('.jpeg', '.jpg')
+
+    # Cargar el modelo
+    model = load_model(model_path)
+
+    # Procesar cada imagen en el directorio
+    for nombre_archivo in os.listdir(directorio_imagenes):
+        if nombre_archivo.lower().endswith(extensiones):
+            img_path = os.path.join(directorio_imagenes, nombre_archivo)
+            img = load_image(img_path)
+
+            # Detección en la imagen original
+            detections = detect_objects(model, img)
+            best_detection = get_best_detection(detections)
+
+            if best_detection is not None:
+                coords = list(map(int, best_detection[:4]))
+                img_crop = recortar_imagen_con_coordenadas(img, coords)
+
+                # Enderezar la imagen de la matrícula
+                img_enderezada = procesar_y_enderezar_letras(img_crop)
+
+                # Volver a aplicar YOLO sobre la imagen enderezada
+                detections_enderezada = detect_objects(model, img_enderezada)
+                best_detection_enderezada = get_best_detection(detections_enderezada)
+
+                if best_detection_enderezada is not None:
+                    # Obtener las coordenadas
+                    coords_enderezada = list(map(int, best_detection_enderezada[:4]))
+
+                    # Recortar la imagen final
+                    img_final_crop = recortar_imagen_con_coordenadas(img_enderezada, coords_enderezada, margen=50)
+                else:
+                    # Si no hay detección, usar la imagen enderezada directamente
+                    img_final_crop = img_enderezada
+                
+                # Procesar la imagen final
+                img_final_enderezada = procesar_y_enderezar_letras(img_final_crop)
+
+                # Guardar la imagen en el directorio especificado
+                img_output_path = os.path.join(directorio_imagenes, 'imatges_matriculas', nombre_archivo)
+                if not os.path.exists(os.path.dirname(img_output_path)):
+                    os.makedirs(os.path.dirname(img_output_path))
+
+                cv2.imwrite(img_output_path, img_final_enderezada)
+                print(f"Imagen guardada en: {img_output_path}")
+                
+            else:
+                print(f"No se detectó ninguna matrícula en la imagen original: {nombre_archivo}")
+
+if __name__ == "__main__":
+    main()
